ba27b6ddbbb0d52176203df9c52e13eedbe30167
Henrique Calheiros <163991285+Raik4@users.noreply.github.com>
2026-05-22 15:55:57 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* Service Worker — Postos JP  v20260522152623 */
-var CACHE = 'postos-jp-20260522152623';
+/* Service Worker — Postos JP  v20260522154851 */
+var CACHE = 'postos-jp-20260522154851';
 var RESOURCES = ['./', 'index.html'];
 
 self.addEventListener('install', function(e) {
